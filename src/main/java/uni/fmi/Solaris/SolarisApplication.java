f9499f0989fa186ef22c2479bc67492edfe31098
Pencho Malinov <pepo.malinov@gmail.com>
2023-10-22 09:11:30 +0300

    init project

---
+++ src/main/java/uni/fmi/Solaris/SolarisApplication.java
@@ -0,0 +1,13 @@
+package uni.fmi.Solaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarisApplication.class, args);
+	}
+
+}
